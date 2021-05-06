bd675d99f8a5292259a72947522f9b684ecc910f
tripleslicer <williamrevans@gmail.com>
2021-05-05 19:09:22 -0700

    add image and change properties

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ const customStyle = {
   border: "1px solid black"
 };
 
+const imageStyle = {
+  height: "100px",
+  width: "100px"
+};
+
 customStyle.color = "blue";
+customStyle.fontSize = "smaller";
 
 ReactDOM.render(
-  <h1 style={customStyle}>Hello World!</h1>,
+  <div>
+    <img
+      className="food-img"
+      style={imageStyle}
+      alt="taco town"
+      src="https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1502767705602-P34YV2BMBOWRXOKB7Z7W/ke17ZwdGBToddI8pDm48kKDKtHioz13A6gTD6cP2SJZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0ooWhOa5cxQSJsU3rXf8luX33ZLveKUc0IfIDXzCtWBDa4GLkaq2lSHIaS7p2YxCXg/tacotown.jpg?format=2500w"
+    ></img>
+    <h1 style={customStyle}>Hello World!</h1>
+  </div>,
   document.getElementById("root")
 );
